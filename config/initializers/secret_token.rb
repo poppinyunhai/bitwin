c51a55744318c97fbcef4662a78f5c3cc52b2af1
songjiayang <songjiayang1@gmail.com>
2013-08-22 20:36:17 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitwin::Application.config.secret_key_base = '351e1af9d2ed4f4b16982d77421f2f76e6b19476ac84fdfc88fced2039d3bf8652778003ddc0b13f24f7227e7e955621b0b5313f41a6e0a9a2df98a86b6bd039'
