70aa7f3b8e5fdb06ed0e021e2014821d067961ee
尹常鑫 <ycxxww@gmail.com>
2013-09-17 22:25:42 +0800

    sms 绑定成功后，跳转

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,6 +67,7 @@ class UsersController < ApplicationController
       current_user.mobile = session[:mobile]
       @result_state = current_user.save!
       @info = "绑定成功！"
+      redirect_to 'account#celphone'
     else
       @error = "验证码有误, 绑定失败, 请重试!"
     end
