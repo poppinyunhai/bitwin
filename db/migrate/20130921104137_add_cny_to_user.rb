8660771caa1a28e0b9c80301f5443d5857fb4900
songjiayang <songjiayang1@gmail.com>
2013-09-22 19:20:45 +0800

    删除没有用的文件

--- db/migrate/20130921104137_add_cny_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddCnyToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :cny, :decimal,
-        :precision => 16,
-        :scale => 8,
-        :default => 0,
-        :null => false
-  end
-end
