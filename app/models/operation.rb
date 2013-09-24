59a2405b7a73a1fc21a686e7018e82c590cfcbff
songjiayang <sjy@gigabase.org>
2013-09-24 10:25:43 +0800

    merge master

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -10,36 +10,36 @@ class Operation < ActiveRecord::Base
 	def self.synchronize_transactions!(type='btc')
 		if type == 'btc'
 			btc_synchronize!
-		end 
+		end
 	end
 
 
 	def self.btc_synchronize!
-		User.all.each do |user|
-      transactions = Bitcoin::Client.instance.list_transactions('btc' + user.id.to_s)
+         User.all.each do |user|
+            transactions = Bitcoin::Client.instance.list_transactions('btc' + user.id.to_s)
 
-      transactions = transactions.select do |tx|
-        ["receive", "generated"].include? tx["category"]
-      end
+            transactions = transactions.select do |tx|
+              ["receive", "generated"].include? tx["category"]
+            end
 
-      transactions.each do |tx|
-        o = Operation.where( :conditions => ['txid = ? AND user_id = ?', tx["txid"], user.id] ).first
-      	if o
-      		o.update_attribute(:confirmations, tx["confirmations"])
-      	else
-      		o = Operation.new
-      		o.user = user
-      		o.category = (tx["category"]=='receive' ? OperationCategory::RECEIVE : OperationCategory::GENERATED)
-      		o.amount = tx['amount']
-      		o.confirmations = tx['confirmations']
-      		o.blockhash = tx['blockhash']
-      		o.blocktime = tx['blocktime']
-      		o.timereceived = tx['timereceived']
-      		o.txid = tx['txid']
-      		o.type = 'btc'
-      		o.save!
-      	end
-      end
+            transactions.each do |tx|
+              o = Operation.where( :conditions => ['txid = ? AND user_id = ?', tx["txid"], user.id] ).first
+            	if o
+            		o.update_attribute(:confirmations, tx["confirmations"])
+            	else
+            		o = Operation.new
+            		o.user = user
+            		o.category = (tx["category"]=='receive' ? OperationCategory::RECEIVE : OperationCategory::GENERATED)
+            		o.amount = tx['amount']
+            		o.confirmations = tx['confirmations']
+            		o.blockhash = tx['blockhash']
+            		o.blocktime = tx['blocktime']
+            		o.timereceived = tx['timereceived']
+            		o.txid = tx['txid']
+            		o.type = 'btc'
+            		o.save!
+            	end
+            end
 	  end
 	end
 
