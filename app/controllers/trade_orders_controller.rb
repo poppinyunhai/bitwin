378212160127363d7024c946a1c9013cda6fbae5
尹常鑫 <ycxxww@gmail.com>
2013-09-28 17:21:00 +0800

    添加委托订单删除

--- app/controllers/trade_orders_controller.rb
+++ app/controllers/trade_orders_controller.rb
@@ -3,7 +3,17 @@ class TradeOrdersController < ApplicationController
   def create
   	trade_order = TradeOrder.new(trade_order_params.merge(user: current_user, blank_account: current_user.cny_account))
   	trade_order.save
-  	redirect_to user_deals_path
+  	redirect_to user_deals_path, notice: '创建委托订单成功'
+  end
+
+  def destroy
+    trade_order = TradeOrder.find(params[:id])
+    if trade_order
+      trade_order.close
+      redirect_to user_deals_path, notice: '关闭委托订单成功'
+    else
+      redirect_to user_deals_path, notice: '关闭委托订单失败'
+    end
   end
 
 private
